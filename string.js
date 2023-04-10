591711ed226e7e7998b34b3a6f3866fdd13c0dac
vishnu-manimala <71614068+vishnu-manimala@users.noreply.github.com>
2023-04-10 07:00:01 +0530

    Add files via upload

---
+++ string.js
@@ -0,0 +1,18 @@
+const prompt = require('prompt-sync')();
+// console.log("String Access");
+// const str = "hi what are you doing?"
+// console.log(str.charAt(3));
+// console.log(str.substring(8,11));
+// console.log(str.split());
+// console.log(str.split());
+
+
+// console.log("Paliandrome");
+// let str1= prompt("enter a string to check whether it is paliandrome or not :- ");
+// let str2 = str1.split("").reverse().join("");
+// console.log(str1 === str2);
+// console.log("\nChange first and last charachet to uppercase");
+
+// let str = "hello world";
+// str = str.charAt(0).toUpperCase() + str.slice(1, -1) + str.slice(-1).toUpperCase();
+// console.log(str);
